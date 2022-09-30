3ef6973f80bfa07eb553bf5e5d4ef37c059da189
decocco <jeremy.galarce.u@gmail.com>
2022-09-29 23:09:56 -0300

    ✨ fix: add coming-soon component

---
+++ src/components/commons/ComingSoon/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+export const ComingSoon: React.FC = () => (
+  <Text
+    color="black"
+    fontFamily="italic"
+    fontSize="30px"
+    position="relative"
+    top="2px"
+  >
+    Coming Soon
+  </Text>
+);
